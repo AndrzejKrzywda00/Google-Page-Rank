d7326d81f2eb63e8d60660c47c4d71da1dc3ded9
AndrzejKrzywda00 <a.krzywda00@gmail.com>
2021-01-25 16:01:33 +0100

    Dodano klasę w Javie do przechowywania zapytania do wyszukiwarki

---
+++ netPagesQuery.java
@@ -0,0 +1,9 @@
+/* This code describes class which holds data for simple net Lookup query
+* It consists of two columns in which i hold pairs of metric of importance and name of the sites
+* */
+
+public class netPagesQuery {
+
+    private ArrayList<String,double> listOfQueries = new TreeMap<String,double>();
+
+}
