434a7da3c3cf7f40f057f7613162d57f2c14206b
AndrzejKrzywda00 <a.krzywda00@gmail.com>
2021-01-26 16:23:26 +0100

    Dodano funkcję kompresjującą macierz do modułu functionsContainer * wymaga poprawy

--- functionsContainer.m
+++ functionsContainer.m
@@ -8,31 +8,52 @@ classdef functionsContainer
     
     methods
         
-        %% funkcja generująca rzadką macierz relacji między stronami internetowymi
-        % spełnia warunki zawarte w głównym skrypcie "PageRank.m"
+        %% Funkcja zapisująca macierz sparse NxN do skompresowanego formatu
+        % można ją wykorzystać przy zapisywaniu macierzy stochastycznej
+        % macierz jest konwertowana do wektora, o krótszej długości
+        % macierz musi spełniać warunek nieujemności wartości zapisanych
 
-        function [genMatrix] = generateWebMatrix(N)
+        % Uwaga! funkcja kompresuje macierz liniowo od górnego lewego rogu
+        % przez kolejne kolumny
+        % następnie zmienia wiersz
+        % funkcja jest czuła na ciągi dłuższe niż 3 zera
 
-            % pierwszym krokiem jest zerowanie lewej górnej przekątnej
-            % żaden węzeł nie ma referencji do samego siebie
-            
-            M = zeros(N);
+        % symbolem wskazującym na początek skompresowanego fragmentu jest -1
+        % następnie następuje informacja jak wiele zer znajduje się w ciągu
+        % następnie dopisywany jest numer sekwencyjny, dzięki niemu można sprawdzić
+        % czy np. odtworzono poprawnie zaszyfrowany 
 
-            % dla każdej kolumny losuję ile będzie połączeń -- liczba z zakresu od
-            % 1 do N
+        function [compresionLevel,outputVector] = compressMatrix(M)
+            zeroCounter = 0;                % parametr zliczający ile zer w danym ciągu wystąpi
+            patchIndicator = -1;            % stała wartość -- symbol charakterystyczny dla kompresji
+            sequenceNumber = 1;
+            outputVector = zeros(1,3);
+
             for i = 1:N
-                howManyConnections = randi(N);              % losuję ile będzie połączeń
-                indexes = randperm(N,howManyConnections);   % ustalam listę bez powtórzeń, na jakich indeksach wystąpią połączenia
-                for j = 1:howManyConnections
-                    M(i,indexes(i)) = 1/N;                  % przypisuję odpowiednim kolumnom ustaloną stałą wartość
-                end      
+                for j = 1:N
+                    if ( M(i,j) <= tolerance )
+                        zeroCounter = zeroCounter + 1;      % to znaczy że trafiłem w zero
+                    else
+                        if ( zeroCounter > 3 )
+                            outputVector = [outputVector,patchIndicator,zeroCounter,sequenceNumber];
+                            zeroCounter = 0;
+                            sequenceNumber = sequenceNumber + 1;
+                        else
+                            for k = 1:zeroCounter
+                                outputVector = [outputVector,0];
+                            end
+                            zeroCounter = 0;
+                        end
+                        
+                    end
+                    
+                    if ( zeroCounter > 3 ) 
+                        
+                    end
+                end     
             end
-    
-            % graphical check
-            HeatMap(M);
-            
-            genMatrix = 3;
-            
-        end
+
+        end 
+
     end
 end
